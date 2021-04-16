544bcd10b15c457f8c0f2ba9cbd2d71f4a69d9e6
Luke Teal <l.teal21@gmail.com>
2021-04-16 10:35:50 -0400

    Add dates to forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,21 @@
 let searchButton = document.querySelector('.search')
 let todayEl = document.querySelector('.today')
-let forecastEls = $('.forecast')
+let forecastAr = $('.forecast')
 
 console.log(searchButton)
-console.log(forecastEls)
+console.log(forecastAr)
 
 
 
 // load date to header
 $('#currentDay').text(dayjs().format('MMMM D, YYYY'));
 
+for(let i=0; i<forecastAr.length; i++) {
+    $(forecastAr[i]).find('p').text(dayjs().add((i+1),'day').format('MMMM D, YYYY'))
+}
+
+
+console.log
 
 let apiKey = '0b34c0c779002825da1931b61289722d'
 
@@ -46,7 +52,7 @@ function fetchWeatherData(event) {
         .then(function (forecastWeather){
             console.log(forecastWeather)
             $(todayEl).append($('<ul><li>Temparature: '+forecastWeather.current.temp+'</li></ul>'))
-            forecastEls.eq(0).append($('<ul><li>Temparature: '+forecastWeather.current.temp+'</li></ul>'))
+            forecastAr.eq(0).append($('<ul><li>Temparature: '+forecastWeather.current.temp+'</li></ul>'))
         })
     })
 
