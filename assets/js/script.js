5a47fe085a8ebc4d42454ded986b71dae6caeb6e
Luke Teal <l.teal21@gmail.com>
2021-04-16 15:06:06 -0400

    Add code to update weather info Add placeholders for data in html, Add JS to populate weather info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,19 @@
 let searchButton = document.querySelector('.search')
 let todayEl = document.querySelector('.today')
 let forecastAr = $('.forecast')
+let dateAr = $('.date')
 
 console.log(searchButton)
 console.log(forecastAr)
 
-
-
 // load date to header
 $('#currentDay').text(dayjs().format('MMMM D, YYYY'));
 
-for(let i=0; i<forecastAr.length; i++) {
-    $(forecastAr[i]).find('p').text(dayjs().add((i+1),'day').format('MMMM D, YYYY'))
+for(let i=0; i<dateAr.length; i++) {
+    dateAr.eq(i).text(dayjs().add((i+1),'day').format('MMMM D, YYYY'))
 }
 
-
-console.log
-
 let apiKey = '0b34c0c779002825da1931b61289722d'
-
 
 // functions
 
@@ -39,7 +34,7 @@ function fetchWeatherData(event) {
 
         console.log(currentWeather)
         let lat = currentWeather.coord.lat
-        let long = currentWeather.coord.lat
+        let long = currentWeather.coord.lon
         console.log(lat)
         let forecastURL = 'https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+long+'&exclude=minutely,hourly,alerts&appid='+apiKey+'&units=imperial'
         console.log(forecastURL)
@@ -51,8 +46,30 @@ function fetchWeatherData(event) {
         })
         .then(function (forecastWeather){
             console.log(forecastWeather)
-            $(todayEl).append($('<ul><li>Temparature: '+forecastWeather.current.temp+'</li></ul>'))
-            forecastAr.eq(0).append($('<ul><li>Temparature: '+forecastWeather.current.temp+'</li></ul>'))
+            let weatherToday= [
+                currentWeather.name, 
+                'Temperatures '+forecastWeather.current.temp+' \u00B0F',
+                'Wind Speed: '+forecastWeather.current.wind_speed+' mph',
+                'Humidity: '+forecastWeather.current.humidity+'%',
+                'UV Index: '+forecastWeather.current.uvi,
+            ]
+    
+            for(let i=0; i<weatherToday.length; i++) {
+                $($(todayEl).find('p')[i]).text(weatherToday[i])
+            }   
+
+            for(let i=0; i<forecastAr.length; i++) {
+                let weatherAr = $(forecastAr[i]).find('p')
+                let weatherTxt = [
+                    'Temperature: '+forecastWeather.daily[i+1].temp.day+' \u00B0F',
+                    'Wind Speed: '+forecastWeather.daily[i+1].wind_speed+' mph',
+                    'Humidity: '+forecastWeather.daily[i+1].humidity+'%'
+                ]
+                for(let j=1, k=0; j<weatherAr.length; j++, k++) {
+                    weatherAr.eq(j).text(weatherTxt[k])
+                  }
+                 console.log(weatherAr)
+            }
         })
     })
 
