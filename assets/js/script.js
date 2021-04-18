e074304ed40f0c9b12bdbfb098bc2635c926dd4a
Luke Teal <l.teal21@gmail.com>
2021-04-17 21:21:06 -0400

    Remove state option, update current weather Remove state option from form and associated js Add weather icons Add colors to current weather Add error handling of fetch Add .trim to input Add local storage functionality and associated buttons Add event listener for history buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,15 @@ let searchButton = document.querySelector('.search')
 let todayEl = document.querySelector('.today')
 let forecastAr = $('.forecast')
 let dateAr = $('.date')
+let historyEl = $('.history')
+let iconURL =  'http://openweathermap.org/img/wn/'
+let apiKey = '0b34c0c779002825da1931b61289722d'
 
-
-
-console.log(searchButton)
-console.log(forecastAr)
+// get search history from local storage
+let searchHistory = JSON.parse(localStorage.getItem('searchHistory'));
+if(searchHistory != null) {
+    printHistory()
+}
 
 // load date to header
 $('#currentDay').text(dayjs().format('MMMM D, YYYY'));
@@ -14,8 +18,6 @@ $('#currentDay').text(dayjs().format('MMMM D, YYYY'));
 for(let i=0; i<dateAr.length; i++) {
     dateAr.eq(i).text(dayjs().add((i+1),'day').format('MMMM D, YYYY'))
 }
-
-let apiKey = '0b34c0c779002825da1931b61289722d'
 
 // load state dropdown
 let stateList=['AK', 'AL', 'AR', 'AS', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA', 'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM', 'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY']
@@ -28,63 +30,117 @@ for(let i=0; i<stateList.length; i++) {
 
 function fetchWeatherData(event) {
     event.preventDefault();
-    let searchText = document.querySelector('#searchText').value
-    let stateText = document.querySelector('#inputState').value
-    console.log(stateText)
+    let searchText = document.querySelector('#searchText').value.trim()
+    $('img').remove()
     console.log(encodeURIComponent(searchText))
     
-    let currentURL = 'https://api.openweathermap.org/data/2.5/weather?q='+encodeURIComponent(searchText)+','+stateText+',US&appid='+apiKey+'&units=imperial'
+    let currentURL = 'https://api.openweathermap.org/data/2.5/weather?q='+encodeURIComponent(searchText)+'&appid='+apiKey+'&units=imperial'
     
     console.log(currentURL)
     fetch(currentURL)
     .then(function (response) {
         console.log(response.status)
+        if (!response.ok) {
+            alert('City not found... Try again')
+        }
         return response.json();
     })
     .then(function (currentWeather){
 
         console.log(currentWeather)
-        let lat = currentWeather.coord.lat
-        let long = currentWeather.coord.lon
-        console.log(lat)
-        let forecastURL = 'https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+long+'&exclude=minutely,hourly,alerts&appid='+apiKey+'&units=imperial'
-        console.log(forecastURL)
+        if(currentWeather.cod === 200) {
+            let lat = currentWeather.coord.lat
+            let long = currentWeather.coord.lon
+            console.log(lat)
+            let forecastURL = 'https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+long+'&exclude=minutely,hourly,alerts&appid='+apiKey+'&units=imperial'
+            console.log(forecastURL)
 
-        fetch(forecastURL)
-        .then(function (response){
-            console.log(response.status)
-            return response.json();
-        })
-        .then(function (forecastWeather){
-            console.log(forecastWeather)
-            let weatherToday= [
-                currentWeather.name, 
-                'Temperatures '+forecastWeather.current.temp+' \u00B0F',
-                'Wind Speed: '+forecastWeather.current.wind_speed+' mph',
-                'Humidity: '+forecastWeather.current.humidity+'%',
-                'UV Index: '+forecastWeather.current.uvi,
-            ]
-    
-            for(let i=0; i<weatherToday.length; i++) {
-                $($(todayEl).find('p')[i]).text(weatherToday[i])
-            }   
-
-            for(let i=0; i<forecastAr.length; i++) {
-                let weatherAr = $(forecastAr[i]).find('p')
-                let weatherTxt = [
-                    'Temperature: '+forecastWeather.daily[i+1].temp.day+' \u00B0F',
-                    'Wind Speed: '+forecastWeather.daily[i+1].wind_speed+' mph',
-                    'Humidity: '+forecastWeather.daily[i+1].humidity+'%'
+            fetch(forecastURL)
+            .then(function (response){
+                console.log(response.status)
+                return response.json();
+            })
+            .then(function (forecastWeather){
+                console.log(forecastWeather)
+                let weatherToday= [
+                    currentWeather.name, 
+                    'Temperatures '+forecastWeather.current.temp+' \u00B0F',
+                    'Wind Speed: '+forecastWeather.current.wind_speed+' mph',
+                    'Humidity: '+forecastWeather.current.humidity+'%',
+                    'UV Index: '+forecastWeather.current.uvi,
                 ]
-                for(let j=1, k=0; j<weatherAr.length; j++, k++) {
-                    weatherAr.eq(j).text(weatherTxt[k])
-                  }
-                 console.log(weatherAr)
-            }
-        })
+        
+                for(let i=0; i<weatherToday.length; i++) {
+                    $($(todayEl).find('p')[i]).text(weatherToday[i])
+                }   
+                $('#cityName').append($('<img src=\''+iconURL+currentWeather.weather[0].icon+'.png\'>'))
+                
+                if(forecastWeather.current.uvi<3) {
+                    $('.uv').attr('class','bg-success text-light text-center rounded-pill').addClass('uv col-2')
+                }
+
+                if(forecastWeather.current.uvi>=3&&forecastWeather.current.uvi<8) {
+                    $('.uv').attr('class','bg-warning text-center rounded-pill').addClass('uv col-2')
+                }
+
+                if(forecastWeather.current.uvi>=8) {
+                    $('.uv').attr('class','bg-danger text-light text-center rounded-pill').addClass('uv col-2')
+                }
+
+                for(let i=0; i<forecastAr.length; i++) {
+                    let weatherAr = $(forecastAr[i]).find('p')
+                    let weatherTxt = [
+                        'Temperature: '+forecastWeather.daily[i+1].temp.day+' \u00B0F',
+                        'Wind Speed: '+forecastWeather.daily[i+1].wind_speed+' mph',
+                        'Humidity: '+forecastWeather.daily[i+1].humidity+'%'
+                    ]
+                    dateAr.eq(i).append($('<img src=\''+iconURL+forecastWeather.daily[i+1].weather[0].icon+'.png\'>'))
+                    for(let j=1, k=0; j<weatherAr.length; j++, k++) {
+                        weatherAr.eq(j).text(weatherTxt[k])
+                        
+                        
+                    }
+                    console.log(weatherAr)
+                }
+                if(searchHistory===null) {
+                    searchHistory = [currentWeather.name]
+                    console.log(searchHistory)
+                    localStorage.setItem('searchHistory', JSON.stringify(searchHistory))
+                } else {
+                    for(let each of searchHistory) {
+                        if(each === currentWeather.name) {
+                            console.log(searchHistory)
+                            printHistory()
+                            return
+                        }
+                    }
+                    searchHistory.push(currentWeather.name)
+                    console.log(searchHistory)
+                    localStorage.setItem('searchHistory', JSON.stringify(searchHistory))
+                }
+                printHistory()
+            })
+        }
     })
 
 }
 
+function printHistory(){
+    $(historyEl).empty()
+    for(let each of searchHistory){
+        $(historyEl).append($('<button>'+each+'</button>')).children().attr("class","btn btn-primary m-2 historyItem")
+    }
+    let historyAr = $('.historyItem');
+    console.log(historyAr)
+    return historyAr
+}
+
+function addHistory(event) {
+    document.querySelector('#searchText').value = event.currentTarget.innerHTML
+    fetchWeatherData(event)
+}
+
+
 // event listeners
 searchButton.addEventListener('click', fetchWeatherData)
+historyEl.on('click','button',addHistory)
